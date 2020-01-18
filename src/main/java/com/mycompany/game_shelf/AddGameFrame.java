9b38fcfcbc5313dd7fe5266bc157fdfcbef1c611
ggulker <ceagentman17@gmail.com>
2020-01-18 04:48:53 -0600

    Made the add game frame look way better and am almost done then continues work on the main frame of the app

--- src/main/java/com/mycompany/game_shelf/AddGameFrame.java
+++ src/main/java/com/mycompany/game_shelf/AddGameFrame.java
@@ -19,30 +19,45 @@ public class AddGameFrame extends javax.swing.JFrame {
      */
     public AddGameFrame() {
         initComponents();
+        gameDesc.setLineWrap(true);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        listPanel = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         gameList = new javax.swing.JList<>();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        gameDesc = new javax.swing.JTextPane();
-        devLabel = new javax.swing.JLabel();
+        background = new javax.swing.JPanel();
+        infoPanel = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        gameDesc = new javax.swing.JTextArea();
         pubLabel = new javax.swing.JLabel();
+        devLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setBackground(new java.awt.Color(153, 153, 153));
+        setBackground(new java.awt.Color(35, 39, 42));
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
 
-        gameList.setFont(new java.awt.Font("Serif", 0, 18)); // NOI18N
+        listPanel.setBackground(new java.awt.Color(44, 47, 51));
+        listPanel.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jScrollPane1.setBackground(new java.awt.Color(44, 57, 41));
+        jScrollPane1.setBorder(null);
+
+        gameList.setBackground(new java.awt.Color(35, 39, 42));
+        gameList.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        gameList.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        gameList.setForeground(new java.awt.Color(153, 153, 153));
         gameList.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "The Witcher 3: Wild Hunt", "Anthem" };
+            String[] strings = { "The Witcher 3: Wild Hunt", "Anthem", "Persona 5" };
             public int getSize() { return strings.length; }
             public String getElementAt(int i) { return strings[i]; }
         });
         gameList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         gameList.setToolTipText("");
+        gameList.setSelectionBackground(new java.awt.Color(204, 204, 204));
         gameList.setVerifyInputWhenFocusTarget(false);
         gameList.setVisibleRowCount(10);
         gameList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
@@ -52,47 +67,101 @@ public class AddGameFrame extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(gameList);
 
-        jScrollPane2.setViewportView(gameDesc);
+        javax.swing.GroupLayout listPanelLayout = new javax.swing.GroupLayout(listPanel);
+        listPanel.setLayout(listPanelLayout);
+        listPanelLayout.setHorizontalGroup(
+            listPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(listPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        listPanelLayout.setVerticalGroup(
+            listPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(listPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 644, Short.MAX_VALUE)
+                .addContainerGap())
+        );
 
-        devLabel.setText("Developer(s) ");
+        getContentPane().add(listPanel);
 
+        background.setBackground(new java.awt.Color(34, 39, 42));
+        background.setForeground(new java.awt.Color(34, 39, 42));
+
+        infoPanel.setBackground(new java.awt.Color(44, 47, 51));
+        infoPanel.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jScrollPane3.setBackground(new java.awt.Color(44, 47, 51));
+        jScrollPane3.setBorder(null);
+
+        gameDesc.setEditable(false);
+        gameDesc.setBackground(new java.awt.Color(34, 39, 42));
+        gameDesc.setColumns(20);
+        gameDesc.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        gameDesc.setForeground(new java.awt.Color(153, 153, 153));
+        gameDesc.setRows(10);
+        gameDesc.setToolTipText("");
+        gameDesc.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        gameDesc.setDisabledTextColor(new java.awt.Color(44, 47, 51));
+        gameDesc.setSelectionColor(new java.awt.Color(44, 47, 51));
+        jScrollPane3.setViewportView(gameDesc);
+
+        javax.swing.GroupLayout infoPanelLayout = new javax.swing.GroupLayout(infoPanel);
+        infoPanel.setLayout(infoPanelLayout);
+        infoPanelLayout.setHorizontalGroup(
+            infoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(infoPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane3)
+                .addContainerGap())
+        );
+        infoPanelLayout.setVerticalGroup(
+            infoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, infoPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane3)
+                .addContainerGap())
+        );
+
+        pubLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        pubLabel.setForeground(new java.awt.Color(153, 153, 153));
         pubLabel.setText("Publisher(s)");
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+        devLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        devLabel.setForeground(new java.awt.Color(153, 153, 153));
+        devLabel.setText("Developer(s) ");
+
+        javax.swing.GroupLayout backgroundLayout = new javax.swing.GroupLayout(background);
+        background.setLayout(backgroundLayout);
+        backgroundLayout.setHorizontalGroup(
+            backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(infoPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, backgroundLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 321, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(devLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(pubLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 276, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 262, Short.MAX_VALUE))
-                    .addComponent(jScrollPane2))
+                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(pubLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, backgroundLayout.createSequentialGroup()
+                        .addComponent(devLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 415, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 316, Short.MAX_VALUE)))
                 .addContainerGap())
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(39, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(341, 341, 341)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(devLabel)
-                            .addComponent(pubLabel))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 523, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+        backgroundLayout.setVerticalGroup(
+            backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, backgroundLayout.createSequentialGroup()
+                .addGap(48, 48, 48)
+                .addComponent(pubLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(devLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 382, Short.MAX_VALUE)
+                .addComponent(infoPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        devLabel.getAccessibleContext().setAccessibleName("devLabel");
+        infoPanel.getAccessibleContext().setAccessibleParent(null);
         pubLabel.getAccessibleContext().setAccessibleName("pubLabel");
+        devLabel.getAccessibleContext().setAccessibleName("devLabel");
+
+        getContentPane().add(background);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -104,17 +173,17 @@ public class AddGameFrame extends javax.swing.JFrame {
         boolean found = selected.containsKey(name);
         if(found)
         {
-         value = selected.get(name);  
+            value = selected.get(name);
         }else
         {
-         value = new Game(name);
-         selected.put(name, value);
+            value = new Game(name);
+            selected.put(name, value);
         }
-        
+
         gameDesc.setText(value.getDesc());
         pubLabel.setText(value.getPub());
         devLabel.setText(value.getDev());
-        
+
         try
         {
             BufferedImage cover = ImageIO.read(value.getImageUrl());
@@ -131,16 +200,19 @@ public class AddGameFrame extends javax.swing.JFrame {
     private void PaintCover(Graphics g)
     {
         Graphics2D draw = (Graphics2D)g;
-        int x = 600;
-        int y = 75;
+        int x = 755;
+        int y = 90;
         draw.drawImage(currentCover, x, y, this);
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel background;
     private javax.swing.JLabel devLabel;
-    private javax.swing.JTextPane gameDesc;
+    private javax.swing.JTextArea gameDesc;
     private javax.swing.JList<String> gameList;
+    private javax.swing.JPanel infoPanel;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JPanel listPanel;
     private javax.swing.JLabel pubLabel;
     // End of variables declaration//GEN-END:variables
 }
